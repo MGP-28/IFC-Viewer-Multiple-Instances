917a0086adf0d954a15b35463027729e4d7a4ad6
joaopereira28 <joaosilvapereira96@hotmail.com>
2022-11-22 15:39:03 +0000

    Implemented model highighting only when user is not moving camera, to increase rendering performance

--- src/components/events/userInputs.js
+++ src/components/events/userInputs.js
@@ -1,17 +1,22 @@
 import emitGlobalEvent from "../../helpers/emitEvent";
 import { pick } from "../../helpers/raytracing";
 
+let isMouseDragging = false;
+
 export default function startUserInputs() {
   document.addEventListener("wereReady", () => {
     const canvas = document.getElementById("three-canvas");
 
-    emitGlobalEvent("loadingComplete")
-    
+    emitGlobalEvent("loadingComplete");
+
     // Double-click => highlights and shows details of pointed object
     canvas.ondblclick = (event) => pick(event, true);
 
     // Mouse move => highlights object being hovered
-    canvas.onmousemove = (event) => pick(event, false);
+    canvas.onmousemove = (event) => { if (!isMouseDragging) pick(event, false); };
 
+    // Prevents highlighting when moving camera (more fluid movement)
+    canvas.onmousedown = (event) => (isMouseDragging = true);
+    canvas.onmouseup = (event) => (isMouseDragging = false);
   });
 }
