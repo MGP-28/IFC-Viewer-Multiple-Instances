d24b4a5d51d4fce7f1ccb9e09d62ffa361780543
joaopereira28 <joaosilvapereira96@hotmail.com>
2022-11-22 14:29:26 +0000

    Possible fix to missaligned models

--- src/helpers/builders/loaderBuilder.js
+++ src/helpers/builders/loaderBuilder.js
@@ -4,9 +4,13 @@ import {
   disposeBoundsTree,
 } from "three-mesh-bvh";
 import { IFCLoader } from "web-ifc-three/IFCLoader";
+import * as THREE from "three";
 
-export default async function setupLoader() {
+let matrix = new THREE.Matrix4();
 
+export const setMatrix = (newMatrix) => matrix = newMatrix;
+
+export async function setupLoader(isFirst) {
   const ifcLoader = new IFCLoader();
 
   ifcLoader.ifcManager.setupThreeMeshBVH(
@@ -16,9 +20,13 @@ export default async function setupLoader() {
   );
 
   await ifcLoader.ifcManager.applyWebIfcConfig({
-    COORDINATE_TO_ORIGIN: true,
+    COORDINATE_TO_ORIGIN: isFirst,
     USE_FAST_BOOLS: false,
   });
+
+  if(!isFirst){
+    ifcLoader.ifcManager.setupCoordinationMatrix(matrix);
+  }
 
   return ifcLoader;
 }
--- src/helpers/loadModels.js
+++ src/helpers/loadModels.js
@@ -3,31 +3,36 @@ import * as Models from "../stores/models.js";
 import Model from "../models/Model.js";
 import { getAllSpacialTrees } from "./spatialTree.js";
 import emitGlobalEvent from "./emitEvent.js";
-import setupLoader from "./builders/loaderBuilder.js";
+import { setupLoader, setMatrix } from "./builders/loaderBuilder.js";
+import * as THREE from "three";
 
 export default async function loadModels(event) {
+  emitGlobalEvent("loading");
 
-  emitGlobalEvent("loading")
-    
   const _ifcLoaders = [];
 
   for (let idx = 0; idx < event.target.files.length; idx++) {
+    const isFirst = idx == 0;
 
     const ifcURL = URL.createObjectURL(event.target.files[idx]);
-    
+
     const modelInstance = new Model();
     Models.addModel(modelInstance);
 
-    const ifcLoader = await setupLoader(ifcLoader);
+    const ifcLoader = await setupLoader(isFirst);
 
-    const loadedModel = await ifcLoader.loadAsync(ifcURL)
+    const loadedModel = await ifcLoader.loadAsync(ifcURL);
+
+    if (isFirst) {
+      const matrixArr = await ifcLoader.ifcManager.ifcAPI.GetCoordinationMatrix(
+        0
+      );
+      const matrix = new THREE.Matrix4().fromArray(matrixArr);
+      setMatrix(matrix);
+    }
+
     Scene.scene.add(loadedModel);
     modelInstance.model = loadedModel;
-
-    // ifcLoader.load(ifcURL, (loadedModel) => {
-    //   Scene.scene.add(loadedModel);
-    //   modelInstance.model = loadedModel;
-    // });
 
     _ifcLoaders.push(ifcLoader);
   }
@@ -38,22 +43,73 @@ export default async function loadModels(event) {
   async function waitLoad() {
     if (!Models.isAllModelsLoaded()) {
       setTimeout(() => {
-        waitLoad()
+        waitLoad();
       }, 1000);
     } else {
-      reorderArrays()
-      const result = await getAllSpacialTrees()
+      reorderArrays();
+      const result = await getAllSpacialTrees();
+
       //
-      if(result) emitGlobalEvent("wereReady")
+      //
+      // document.addEventListener("wereReady", () => {
+      //   function distanceVector(v1, v2) {
+      //     var dx = v1.x - v2.x;
+      //     var dy = v1.y - v2.y;
+      //     var dz = v1.z - v2.z;
+
+      //     return Math.sqrt(dx * dx + dy * dy + dz * dz);
+      //   }
+
+      //   console.log(Models.models[0].loader);
+
+      //   const baseModel = Models.models[0].model;
+      //   baseModel.geometry.computeBoundingBox(); // otherwise geometry.boundingBox will be undefined
+
+      //   var boundingBox = baseModel.geometry.boundingBox.clone();
+      //   const center = new THREE.Vector3();
+      //   boundingBox.getCenter(center);
+      //   Scene.scene.add(boundingBox);
+      //   console.log(center);
+
+      //   const geometry = new THREE.BoxGeometry(1, 1, 1);
+      //   const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+      //   const cube = new THREE.Mesh(geometry, material);
+      //   cube.position.addScaledVector(center, 1);
+      //   Scene.scene.add(cube);
+      //   new Scene();
+
+      //   Scene.scene.translateX(-center.x);
+      //   Scene.scene.translateY(-center.y);
+      //   Scene.scene.translateZ(-center.z);
+
+      //   const secondModel = Models.models[1].model;
+      //   secondModel.geometry.computeBoundingBox(); // otherwise geometry.boundingBox will be undefined
+      //   var boundingBox = baseModel.geometry.boundingBox.clone();
+      //   const center2 = new THREE.Vector3();
+      //   boundingBox.getCenter(center2);
+
+      //   const distance = distanceVector(new Vector3(), center)
+      //   const vector = center
+      //   vector.negate();
+
+      //   baseModel.translateOnAxis(vector, distance);
+      // });
+      //
+      //
+      //
+
+      if (result) emitGlobalEvent("wereReady");
     }
   }
 
   function reorderArrays() {
     for (let idx = 0; idx < Models.models.length; idx++) {
       const correspondingLoader = _ifcLoaders.find(
-        (x) => Models.models[idx].model.uuid == x.ifcManager.state.models[0].mesh.uuid
+        (x) =>
+          Models.models[idx].model.uuid ==
+          x.ifcManager.state.models[0].mesh.uuid
       );
-      Models.models[idx].loader = correspondingLoader
+      Models.models[idx].loader = correspondingLoader;
     }
   }
 }
