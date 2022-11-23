fb4ef44fdfcd03bc72c9b13abc9e2b3673522aea
joaopereira28 <joaosilvapereira96@hotmail.com>
2022-11-23 10:45:35 +0000

    Implemented method to create subset for a single object

--- src/helpers/subsetBuilder.js
+++ src/helpers/subsetBuilder.js
@@ -2,7 +2,7 @@ import materials from "../configs/materials.js";
 import * as Scene from "../stores/scene.js";
 
 /**
- * 
+ *
  * @param {Object} model model instance of the loaded IFC
  * @param {array} ids array with all the expressIDs of the object to add to the subset
  * @param {string} type name of type of material to be used (see configs/materials.js)
@@ -20,12 +20,18 @@ function createSubset(model, ids, type) {
 }
 
 function removeSubset(model, type) {
-
-  // model from store, type is string (selected, highlighted, ...)
-  // const material = materials[type];
-  // model.loader.ifcManager.removeSubset(0, Scene.scene, material);
-
-  createSubset(model, [], type)
+  createSubset(model, [], type);
 }
 
-export { createSubset, removeSubset };
+function createObjectSubset(model, expressID) {
+  const subset = model.loader.ifcManager.createSubset({
+    modelID: 0,
+    ids: [expressID],
+    scene: Scene.scene,
+    removePrevious: true,
+    customID: expressID,
+  });
+  return subset;
+}
+
+export { createSubset, removeSubset, createObjectSubset };
