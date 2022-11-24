607d0d9c141ca9a0d0f36954f2c1747570535d48
joaopereira28 <joaosilvapereira96@hotmail.com>
2022-11-24 15:16:02 +0000

    Implemented timer to time how long a model needs to load

---
+++ src/components/events/timer.js
@@ -0,0 +1,37 @@
+export default function startTimer() {
+  let timer = 0; //seconds
+
+  document.addEventListener("loading", () => {
+    timerStart();
+  });
+
+  document.addEventListener("wereReady", () => {
+    timerStop();
+  });
+
+  function timerStart() {
+    setInterval(tickTimer, 100);
+  }
+
+  function tickTimer(){
+    timer++;
+  }
+
+  function timerStop() {
+    let seconds = timer;
+    let minutes = 0;
+    let hours = 0;
+    while (seconds > 59) {
+      seconds -= 60;
+      minutes++;
+    }
+    while (minutes > 59) {
+      minutes -= 60;
+      hours++;
+    }
+    const message = `
+        Time elapsed: ${hours}h ${minutes}m ${seconds}s
+    `;
+    alert(message);
+  }
+}
--- src/components/index.js
+++ src/components/index.js
@@ -4,11 +4,13 @@ import startObjectDetail from "./objectDetail.js";
 import startRenderingEvents from "./events/viewerRenderingEvents.js";
 import startFeatureSidebar from "./feature-sidebar/index.js";
 import startLoadingPopup from "./loading.js";
+import startTimer from "./events/timer.js";
 
 export default function render() {
   startLoadingPopup();
   startInput();
   startUserInputs();
+  startTimer();
   startRenderingEvents();
   startObjectDetail();
   startFeatureSidebar();
