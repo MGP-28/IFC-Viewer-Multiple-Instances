5f46f94eec6f8ab366e4138a913aa8f557b9882f
joaopereira28 <joaosilvapereira96@hotmail.com>
2022-11-24 10:44:04 +0000

    Models.js - Cleaned imports; Commented more extensively about subsets

--- src/models/Model.js
+++ src/models/Model.js
@@ -4,10 +4,10 @@
 // model = IfcModel created and inserted into the scene
 // tree = Spacial tree of the model
 // levels [] = Array of Level instances used in the IFC model. Contains related categories
-// subsets [] = Collection of subsets seperated by category and level. See: subset model
+// subset = Model collection of objects rendered
+//          Remove an object to subset => loader.ifcManager.removeFromSubset(0, [id], customID)
+//          Add an object to subset => .loader.ifcManager.createSubset({ (...), removePrevious: false })
 // name = Model name
-
-import Level from "./SpatialTree/Level";
 
 export default class Model {
   constructor() {
