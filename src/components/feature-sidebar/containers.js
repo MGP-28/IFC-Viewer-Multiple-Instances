f56d9f56c4228912ff52fe2fad1548eb632f1d3c
MGP-28 <joaosilvapereira96@hotmail.com>
2022-11-30 09:30:36 +0000

    Sidebar feature animations for closing/opening tool

--- src/components/feature-sidebar/containers.js
+++ src/components/feature-sidebar/containers.js
@@ -2,8 +2,9 @@ import { createElement } from "../../helpers/generic/domElements";
 import { buildIcon } from "../generic/icon";
 
 function renderFeatureContainer(icon, header, name) {
-
-  const featureContainer = createElement("div", {classes: ["tools-side-feature"]});
+  const featureContainer = createElement("div", {
+    classes: ["tools-side-feature"],
+  });
   featureContainer.innerHTML = `
     <div class="tools-side-feature-icon"></div>
     <div class="tools-side-feature-header">${header}</div>
@@ -12,27 +13,49 @@ function renderFeatureContainer(icon, header, name) {
     <div class="tools-side-feature-content"></div>
     `;
 
-
   const iconClose = buildIcon("x");
-  const closeWrapper = featureContainer.getElementsByClassName("tools-side-feature-close")[0];
-  closeWrapper.classList.add("interaction-cursor")
+  const closeWrapper = featureContainer.getElementsByClassName(
+    "tools-side-feature-close"
+  )[0];
+  closeWrapper.classList.add("interaction-cursor");
   closeWrapper.appendChild(iconClose);
-  
 
   const iconMain = buildIcon(icon);
-  const iconWrapper = featureContainer.getElementsByClassName("tools-side-feature-icon")[0];
-  iconWrapper.classList.add("interaction-cursor")
+  const iconWrapper = featureContainer.getElementsByClassName(
+    "tools-side-feature-icon"
+  )[0];
+  iconWrapper.classList.add("interaction-cursor");
   iconWrapper.appendChild(iconMain);
 
+  let isActive = false;
   iconWrapper.addEventListener("click", (e) => {
-    featureContainer.classList.toggle("feature-active")
-  })
+    isActive = !isActive;
+    animationControl(isActive);
+  });
 
   closeWrapper.addEventListener("click", (e) => {
-    featureContainer.classList.remove("feature-active")
-  })
+    isActive = false;
+    animationControl(isActive);
+  });
+
+  function animationControl(isActive) {
+    if (isActive) {
+      featureContainer.classList.remove("closing");
+      featureContainer.classList.add("feature-active");
+      featureContainer.classList.add("opening");
+      setTimeout(() => {
+        featureContainer.classList.remove("opening");
+      }, 500);
+    } else {
+      featureContainer.classList.add("closing");
+      setTimeout(() => {
+        featureContainer.classList.remove("feature-active");
+        featureContainer.classList.remove("closing");
+      }, 500);
+    }
+  }
 
   return featureContainer;
 }
 
-export { renderFeatureContainer }
+export { renderFeatureContainer };
