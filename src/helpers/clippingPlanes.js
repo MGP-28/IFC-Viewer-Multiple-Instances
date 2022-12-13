196fc7812070439bf89715f65c4a516594cb818f
MGP-28 <joaosilvapereira96@hotmail.com>
2022-12-13 09:35:48 +0000

    Clipping plane alignment correctly implemented

--- src/helpers/clippingPlanes.js
+++ src/helpers/clippingPlanes.js
@@ -6,12 +6,12 @@ import * as ModelStore from "../stores/models";
 import * as SceneStore from "../stores/scene";
 
 const referenceVectors = {
-  x1: new THREE.Vector3(-1, 0, 0).normalize(),
-  x2: new THREE.Vector3(1, 0, 0).normalize(),
-  y1: new THREE.Vector3(0, -1, 0).normalize(),
-  y2: new THREE.Vector3(0, 1, 0).normalize(),
-  z1: new THREE.Vector3(0, 0, -1).normalize(),
-  z2: new THREE.Vector3(0, 0, 1).normalize(),
+  x2: new THREE.Vector3(1, 0, 0),
+  x1: new THREE.Vector3(-1, 0, 0),
+  y2: new THREE.Vector3(0, 1, 0),
+  y1: new THREE.Vector3(0, -1, 0),
+  z2: new THREE.Vector3(0, 0, 1),
+  z1: new THREE.Vector3(0, 0, -1),
 };
 
 export default function clipping() {
@@ -27,6 +27,14 @@ export default function clipping() {
   // get box's min and max vectors
   const vMin = boundingBox.min;
   const vMax = boundingBox.max;
+  bufferEdgeVectors(vMin);
+  bufferEdgeVectors(vMax);
+
+  function bufferEdgeVectors(vector) {
+    for (const axle in vector) {
+      vector[axle] = vector[axle] > 0 ? vector[axle] + 1 : vector[axle] - 1;
+    }
+  }
 
   // Get plane max size
   const boundingBoxSize = new THREE.Vector3();
@@ -38,64 +46,63 @@ export default function clipping() {
   const planes = {
     visual: [],
     cutting: [],
+    edges: []
   };
 
-  let counter = -1;
   for (const key in referenceVectors) {
-    counter++;
-    if (counter > 0) break;
-    // if (counter < 3) continue;
     const vNormal = referenceVectors[key];
     // preset for x1 plane
     let width = boundingBoxSize.z;
     let height = boundingBoxSize.y;
-    let position = vMax.x;
+    let position = vMin.x;
     let angle = "90º";
     let vAxle = referenceVectors.y2;
+    let invertConstant = true;
     // assigns the correct vars for each case
     if (vNormal.x > 0) {
       // values already in preset
     } else if (vNormal.x < 0) {
-      position = vMin.x;
+      position = vMax.x;
       vAxle = referenceVectors.y2;
       angle = "-90º";
+      invertConstant = false;
     } else if (vNormal.y > 0) {
-      position = vMax.y;
+      position = vMin.y;
       vAxle = referenceVectors.x2;
       width = boundingBoxSize.x;
       height = boundingBoxSize.z;
     } else if (vNormal.y < 0) {
-      position = vMin.y;
+      position = vMax.y;
       vAxle = referenceVectors.x2;
       angle = "-90º";
       width = boundingBoxSize.x;
       height = boundingBoxSize.z;
+      invertConstant = false;
     } else if (vNormal.z > 0) {
-      position = vMax.z;
+      position = vMin.z;
       vAxle = referenceVectors.z2;
       width = boundingBoxSize.y;
       height = boundingBoxSize.x;
     } else if (vNormal.z < 0) {
-      position = vMin.z;
+      position = vMax.z;
       vAxle = referenceVectors.z2;
       angle = "-90º";
       width = boundingBoxSize.y;
       height = boundingBoxSize.x;
+      invertConstant = false;
     }
     angle = angle == "90º" ? THREE.Math.degToRad(90) : THREE.Math.degToRad(270);
 
-    buildPlane(vNormal, position, vAxle, angle, width, height);
+    buildPlane(vNormal, position, vAxle, angle, width, height, invertConstant);
   }
-
-  // console.log("visual", planes.visual);
 
   //testing
   const planeGeom = new THREE.PlaneGeometry(1, 1);
   const planeMat = new THREE.MeshBasicMaterial({
-    color: 0xa6cfe2,
+    color: 0xff0088,
     side: THREE.DoubleSide,
     transparent: true,
-    opacity: 0.5,
+    opacity: 0.6,
     depthWrite: false,
   });
   const planeXY = new THREE.Mesh(planeGeom, planeMat);
@@ -104,24 +111,25 @@ export default function clipping() {
 
   // Default plane already occupies XY plane
   planeXY.rotation.set(0, 0, 0);
+  planeXY.scale.addScalar(100);
   planeXY.material.clippingPlanes = planes.cutting;
   SceneStore.scene.add(planeXY);
 
   // Rotate around x-axis to occupy XZ plane
   planeXZ.rotation.set(Math.PI / 2, 0, 0);
+  planeXZ.scale.addScalar(100);
   planeXZ.material.clippingPlanes = planes.cutting;
   SceneStore.scene.add(planeXZ);
 
   // Rotate around y-axis to occupy YZ plane
   planeYZ.rotation.set(0, Math.PI / 2, 0);
+  planeYZ.scale.addScalar(100);
   planeYZ.material.clippingPlanes = planes.cutting;
   SceneStore.scene.add(planeYZ);
   //
   //
 
   updateModelsMaterials();
-
-  assignClippingPlanes();
 
   // #region Auxiliary functions in scope
 
@@ -188,8 +196,17 @@ export default function clipping() {
     //#endregion render box in scene - comment return for it
   }
 
-  function buildPlane(vNormal, position, vAxle, angle, width, height) {
-    //position = position + 1;
+  function buildPlane(
+    vNormal,
+    position,
+    vAxle,
+    angle,
+    width,
+    height,
+    invertConstant
+  ) {
+    // position = position * 2 / 3
+
     const planeMaterial = Materials.materials.transparent;
 
     // visual plane
@@ -205,7 +222,6 @@ export default function clipping() {
       if (vNormal[key] !== 0) visualPlane.position[key] = position;
     }
     visualPlane.material.side = THREE.DoubleSide;
-
     visualPlane.rotateOnWorldAxis(vAxle, angle);
 
     // Gives each cube face a colored edge
@@ -219,39 +235,41 @@ export default function clipping() {
     planes.visual.push(visualPlane);
     SceneStore.scene.add(visualPlane);
 
-    // return;
+    // visual plane edge renderer
+    // const edgePlane = visualPlane.clone(true);
+    // edgePlane.material = Materials.materials.planeStencilMat;
+    // planes.edges.push(edgePlane);
+    // SceneStore.scene.add(edgePlane);
 
     // cutting plane
-
-    const constant = position * -1;
+    const constant = invertConstant ? position * -1 : position;
     const cuttingPlane = new THREE.Plane(vNormal, constant);
-    //
-
-    // const cuttingPlaneMesh = visualPlane.clone(true);
-    // cuttingPlaneMesh.material = Materials.materials.highlighted;
-    // cuttingPlaneMesh.material.side = THREE.DoubleSide;
-    // cuttingPlaneMesh.position = new THREE.Vector3(0, 0, 0);
-    // for (const axle in cuttingPlane.normal) {
-    //   if(cuttingPlane.normal[axle] !== 0) cuttingPlaneMesh.position[axle] = cuttingPlane.constant
-    // }
 
     planes.cutting.push(cuttingPlane);
-
-    // SceneStore.scene.add(cuttingPlaneMesh);
   }
 
   function updateModelsMaterials() {
-    assignClippingPlanes();
+    // assignClippingPlanes();
+
     // assign each cutting plane as a clipping plane of all models
     for (let idx = 0; idx < meshes.length; idx++) {
       const mesh = meshes[idx];
       for (const subMat in mesh.material) {
         mesh.material[subMat].clippingPlanes = planes.cutting;
       }
-      buildMeshRenderTextures(mesh);
     }
+
+    // for (let idx = 0; idx < planes.edges.length; idx++) {
+    //   const element = planes.edges[idx];
+    //   element.material.clippingPlanes = planes.cutting;
+    // }
+
+    // buildMeshRenderTextures();
   }
 
+  /**
+   * UNUSED
+   */
   function assignClippingPlanes() {
     // assing clipping planes to the planes' materials
     Materials.materials.frontFaceStencilMat.clippingPlanes = planes.cutting;
@@ -260,6 +278,7 @@ export default function clipping() {
 
   /**
    * Provides means for stencil rendering on plane cutting (colored cuts)
+   * UNUSED
    * @param {} mesh models' mesh
    */
   function buildMeshRenderTextures(mesh) {
@@ -279,22 +298,4 @@ export default function clipping() {
   }
 
   // #endregion Auxiliary functions in scope
-
-  // test
-
-  const axesHelper = new THREE.AxesHelper(5);
-  SceneStore.scene.add(axesHelper);
-
-  function testShape() {
-    const geometryTest = new THREE.SphereGeometry(0.5, 32, 16);
-    const materialTest = new THREE.MeshBasicMaterial({
-      color: 0xffff00,
-      side: THREE.DoubleSide,
-    });
-    const circleTest = new THREE.Mesh(geometryTest, materialTest);
-    circleTest.position.x = 0;
-    circleTest.position.y = 2;
-    circleTest.position.z = 0;
-    SceneStore.scene.add(circleTest);
-  }
 }
