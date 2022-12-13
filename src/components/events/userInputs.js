886b495e96a5a3485cfdb23119bc9670373ae577
MGP-28 <joaosilvapereira96@hotmail.com>
2022-12-13 10:14:37 +0000

    Disable/enable camera controls on clipping plane drag

--- src/components/events/userInputs.js
+++ src/components/events/userInputs.js
@@ -3,6 +3,7 @@ import { pickObject, pickClippingPlane } from "../../helpers/raytracing";
 import { foundPlane, visualPlanes } from "../../stores/clippingPlanes";
 import { userInteractions } from "../../stores/userInteractions";
 import * as Materials from "../../configs/materials";
+import { controls, scene } from "../../stores/scene";
 
 let isMouseDragging = false;
 
@@ -27,10 +28,8 @@ export default function startUserInputs() {
         }
         // if there's a plane being interacted, enable highlight
         highlightVisualPlane();
-        
       } else pickObject(event, false);
     };
-
 
     let isMovingPlanes = false;
     // Prevents highlighting when moving camera (more fluid movement)
@@ -38,41 +37,51 @@ export default function startUserInputs() {
       isMouseDragging = true;
 
       // clipping plane
-      if(!foundPlane) return
-      
+      if (!foundPlane) return;
+
       isMovingPlanes = true;
-      //
-      // // plane moving logic
-      // disable camera
-      // drag plane
-      //
+
+      // plane moving logic
+      moveClippingPlane();
     };
     canvas.onmouseup = (event) => {
-      (isMouseDragging = false);
+      isMouseDragging = false;
 
       // clipping plane
-      if(!isMovingPlanes) return;
+      if (!isMovingPlanes) return;
 
       isMovingPlanes = false;
-      //
-      // // plane end movement logic
-      // enable camera
-      // fix plane
-      //
-    }
+
+      // plane end movement logic
+      stopMovingClippingPlane();
+    };
   });
 }
 
-function resetVisualPlanesColoring(){
+function resetVisualPlanesColoring() {
   for (let idx = 0; idx < visualPlanes.length; idx++) {
     const visualPlane = visualPlanes[idx];
     visualPlane.material = Materials.materials.transparent.clone();
   }
 }
 
-function highlightVisualPlane(){
+function highlightVisualPlane() {
   resetVisualPlanesColoring();
   const visualPlane = foundPlane.object;
   visualPlane.material.opacity = 0.28;
   visualPlane.material.color = Materials.materials.highlighted.color;
+}
+
+function moveClippingPlane() {
+  // disable camera
+  controls.enabled = false;
+  // drag plane
+
+}
+
+function stopMovingClippingPlane() {
+  // enable camera
+  controls.enabled = true;
+  // stop plane
+
 }
