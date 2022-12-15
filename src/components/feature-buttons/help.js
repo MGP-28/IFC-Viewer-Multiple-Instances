c105b726ce9a92db8966342d5182179d3b7cab6c
MGP-28 <joaosilvapereira96@hotmail.com>
2022-12-15 13:10:21 +0000

    Help button, modal and content implemented; Added relevant styling

--- src/components/feature-buttons/help.js
+++ src/components/feature-buttons/help.js
@@ -1,29 +1,87 @@
+import { HelperTips } from "../../configs/helper";
+import { icons } from "../../configs/icons";
+import { createElement } from "../../helpers/generic/domElements";
+import { buildIcon } from "../generic/icon";
 import { buildModal } from "../generic/modal";
 import { featureButton } from "./button";
 
-export default function renderClippingPlanesFeature() {
-  const element = featureButton("help-square", "Help");
+export default function renderHelperFeature() {
+  const element = featureButton(icons.helper, "Help");
+  element.classList.remove("not-ready");
 
-  
   const popup = buildModal();
   popup.innerHTML = `
-  
-  `
-  
+    <div class="feature-helper-container">
+    </div>
+  `;
+  const container = popup.children[0];
+
+  for (const feature in HelperTips) {
+    const tip = HelperTips[feature];
+    const tipEl = buildTip(tip);
+    container.appendChild(tipEl);
+    const seperator = buildSeperator();
+    container.appendChild(seperator);
+  }
+  // remove last seperator
+  container.lastChild.remove();
+
   let isEnabled = false;
-
-  element.addEventListener("startFeature", (e) => {
-    element.addEventListener("click", (e) => {
-      isEnabled = !isEnabled;
-      element.classList.toggle("active");
-    });
-
-    
+  element.addEventListener("click", (e) => {
+    toggleState();
   });
 
+  popup.addEventListener("click", (e) => {
+    toggleState();
+  });
+  container.addEventListener("click", (e) => {
+    e.stopPropagation();
+  });
+
+  document.body.appendChild(popup);
+
   return element;
+
+  function toggleState() {
+    isEnabled = !isEnabled;
+    popup.classList.toggle("hidden");
+    element.classList.toggle("active");
+  }
 }
 
-function buildTip(title, icon, description){
+function buildTip(tip) {
+  const wrapper = document.createElement("div");
+  wrapper.classList.add("feature-helper-tip-container");
 
+  const icon = buildIcon(tip.icon);
+  icon.classList.add("feature-helper-tip-icon");
+  wrapper.appendChild(icon);
+
+  const title = createElement("h1", {
+    classes: ["feature-helper-tip-title"],
+    textContent: tip.title,
+  });
+  wrapper.appendChild(title);
+
+  const description = createElement("div", {
+    classes: ["feature-helper-tip-description"],
+  });
+  for (let idx = 0; idx < tip.description.length; idx++) {
+    const text = tip.description[idx];
+    const line = createElement("p", {
+      classes: ["feature-helper-tip-description-line"],
+      textContent: text,
+    });
+    description.appendChild(line);
+  }
+  wrapper.appendChild(description);
+
+  return wrapper;
+}
+
+function buildSeperator(){
+    const element = createElement("div", {
+        classes: ["feature-helper-seperator"]
+    })
+    return element;
 }
---
+++ src/configs/helper.js
@@ -0,0 +1,36 @@
+import { icons } from "./icons"
+
+const tips = {
+    helper: {
+        title: "Helper",
+        icon: icons.helper,
+        description: [
+            "Here you can find tips on how to work with this tool"
+        ]
+    },
+    clipping: {
+        title: "Clipping planes",
+        icon: icons.clipping,
+        description: [
+            "Project sized box that can have its faces dragged to cut the presented models",
+            "Drag faces with the mouse",
+            "Hold Left Control and drag for precise movements"
+        ]
+    },
+    properties: {
+        title: "Properties",
+        icon: icons.properties,
+        description: [
+            "Placeholder text",
+        ]
+    },
+    spatialTree: {
+        title: "Spatial Tree",
+        icon: icons.spatialTree,
+        description: [
+            "Placeholder text",
+        ]
+    },
+}
+
+export { tips as HelperTips }
