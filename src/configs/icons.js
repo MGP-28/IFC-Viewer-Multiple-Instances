d36910a783e85aacd53f4a5a981fd300ad66bf2f
MGP-28 <joaosilvapereira96@hotmail.com>
2022-12-15 13:07:17 +0000

    Moved icon directories into a single module

---
+++ src/configs/icons.js
@@ -0,0 +1,13 @@
+const icons = {
+    properties: "building-06",
+    spatialTree: "dataflow-02",
+    clipping: "cube-04",
+    helper: "help-circle",
+    close: "x",
+    chevronRight: "chevron-right",
+    visibility: "eye",
+    target: "target-02",
+    partof: "scale-01"
+}
+
+export { icons }
