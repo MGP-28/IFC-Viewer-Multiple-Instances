29a76b6fd7a05385f502d9d4a11b69c5afd7f154
MGP-28 <joaosilvapereira96@hotmail.com>
2022-12-15 15:31:57 +0000

    Helper popup functionality implemented

--- src/components/feature-buttons/help.js
+++ src/components/feature-buttons/help.js
@@ -1,4 +1,4 @@
-import { HelperTips } from "../../configs/helper";
+import { HelperTips, HelperHeader } from "../../configs/helper";
 import { icons } from "../../configs/icons";
 import { createElement } from "../../helpers/generic/domElements";
 import { buildIcon } from "../generic/icon";
@@ -11,11 +11,22 @@ export default function renderHelperFeature() {
 
   const popup = buildModal();
   popup.innerHTML = `
-    <div class="feature-helper-container">
+    <div class="feature-helper-wrapper">
+        <div class="feature-helper-wrapper-buffer">
+            <div class="feature-helper-container">
+            </div>
+        </div>
     </div>
   `;
-  const container = popup.children[0];
+  const wrapper = popup.getElementsByClassName("feature-helper-wrapper")[0];
+  const container = popup.getElementsByClassName("feature-helper-container")[0];
 
+  // Header
+  const header = buildHeader();
+  // inserts node on top
+  wrapper.insertBefore(header, wrapper.firstChild);
+
+  // Content
   for (const feature in HelperTips) {
     const tip = HelperTips[feature];
     const tipEl = buildTip(tip);
@@ -26,15 +37,16 @@ export default function renderHelperFeature() {
   // remove last seperator
   container.lastChild.remove();
 
-  let isEnabled = false;
-  element.addEventListener("click", (e) => {
-    toggleState();
-  });
+  const closer = header.getElementsByClassName(
+    "feature-helper-header-close"
+  )[0];
 
-  popup.addEventListener("click", (e) => {
-    toggleState();
-  });
-  container.addEventListener("click", (e) => {
+  let isEnabled = false;
+
+  // event handling for opening/closing feature
+  stateHandling([element, popup, closer]);
+
+  wrapper.addEventListener("click", (e) => {
     e.stopPropagation();
   });
 
@@ -42,11 +54,41 @@ export default function renderHelperFeature() {
 
   return element;
 
+  // In-scope aux functions
+  function stateHandling(stateTogglers) {
+    stateTogglers.forEach((element) => {
+      element.addEventListener("click", (e) => {
+        toggleState();
+      });
+    });
+  }
+
   function toggleState() {
     isEnabled = !isEnabled;
     popup.classList.toggle("hidden");
     element.classList.toggle("active");
   }
+}
+
+function buildHeader() {
+  const header = createElement("div", {
+    classes: ["feature-helper-header"],
+  });
+  header.innerHTML = `
+    <div class="feature-helper-header-text">${HelperHeader.title}</div>
+    <div class="feature-helper-header-subtitle">${HelperHeader.description}</div>
+    `;
+
+  // add icons
+  const icon = buildIcon(HelperHeader.icon);
+  icon.classList.add("feature-helper-header-icon");
+  header.insertBefore(icon, header.firstElementChild);
+
+  const close = buildIcon(icons.close);
+  close.classList.add("feature-helper-header-close");
+  header.insertBefore(close, header.lastElementChild);
+
+  return header;
 }
 
 function buildTip(tip) {
@@ -79,9 +121,9 @@ function buildTip(tip) {
   return wrapper;
 }
 
-function buildSeperator(){
-    const element = createElement("div", {
-        classes: ["feature-helper-seperator"]
-    })
-    return element;
+function buildSeperator() {
+  const element = createElement("div", {
+    classes: ["feature-helper-seperator"],
+  });
+  return element;
 }
--- src/configs/helper.js
+++ src/configs/helper.js
@@ -1,36 +1,31 @@
-import { icons } from "./icons"
+import { icons } from "./icons";
 
 const tips = {
-    helper: {
-        title: "Helper",
-        icon: icons.helper,
-        description: [
-            "Here you can find tips on how to work with this tool"
-        ]
-    },
-    clipping: {
-        title: "Clipping planes",
-        icon: icons.clipping,
-        description: [
-            "Project sized box that can have its faces dragged to cut the presented models",
-            "Drag faces with the mouse",
-            "Hold Left Control and drag for precise movements"
-        ]
-    },
-    properties: {
-        title: "Properties",
-        icon: icons.properties,
-        description: [
-            "Placeholder text",
-        ]
-    },
-    spatialTree: {
-        title: "Spatial Tree",
-        icon: icons.spatialTree,
-        description: [
-            "Placeholder text",
-        ]
-    },
-}
+  clipping: {
+    title: "Clipping planes",
+    icon: icons.clipping,
+    description: [
+      "Project sized box that can have its faces dragged to cut the presented models",
+      "Drag faces with the mouse",
+      "Hold Left Control and drag for precise movements",
+    ],
+  },
+  properties: {
+    title: "Properties",
+    icon: icons.properties,
+    description: ["Placeholder text"],
+  },
+  spatialTree: {
+    title: "Spatial Tree",
+    icon: icons.spatialTree,
+    description: ["Placeholder text"],
+  },
+};
 
-export { tips as HelperTips }
+const header = {
+  title: "Helper",
+  icon: icons.helper,
+  description: "Here you can find tips on how to work with this tool"
+};
+
+export { tips as HelperTips, header as HelperHeader };
