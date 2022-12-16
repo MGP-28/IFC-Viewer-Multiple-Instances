480a6b365c46224b7055b520bb5e2669004d3f72
MGP-28 <joaosilvapereira96@hotmail.com>
2022-12-16 11:21:16 +0000

    DEBUG - logging tool created

--- src/helpers/generic/logging.js
+++ src/helpers/generic/logging.js
@@ -1,6 +1,21 @@
-function consoleLogObject(obj, message = ""){
-    if(message) console.log(message, JSON.stringify(obj))
-    else console.log(JSON.stringify(obj))
+function consoleLogObject(obj, message = "") {
+  if (message) console.log(message, JSON.stringify(obj));
+  else console.log(JSON.stringify(obj));
 }
 
-export { consoleLogObject }
+/**
+ *
+ * @param {Object} logs {title (string), objects (array)}
+ */
+function multipleLogsWithSeperator(logs) {
+  const title = logs.title ? log.title : "No title";
+  console.log("----- START -----");
+  console.log(title);
+  console.log("-----------------");
+  logs.objects.forEach((element) => {
+    console.log(element);
+  });
+  console.log("------ END ------");
+}
+
+export { consoleLogObject, multipleLogsWithSeperator };
