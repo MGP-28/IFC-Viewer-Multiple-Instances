f11b22c92f6220656a1a356a22135f1be6b52368
MGP-28 <joaosilvapereira96@hotmail.com>
2022-12-16 11:16:41 +0000

    Implemented camera state saving and loading

--- src/helpers/camera.js
+++ src/helpers/camera.js
@@ -1,28 +1,41 @@
 import { savedViews } from "../stores/savedViews";
 import * as StoreScene from "../stores/scene";
+import { multipleLogsWithSeperator } from "./generic/logging";
+import * as THREE from "three";
 
 function getCameraData() {
   const camera = StoreScene.camera;
 
+  // get rotation
   const vRotation = new THREE.Vector3();
   camera.getWorldDirection(vRotation);
 
+  // get position
   camera.updateMatrixWorld();
   const vPosition = camera.position.clone();
-  vPosition.applyMatrix(camera.matrixWorld);
+  // vPosition.applyMatrix3(camera.matrixWorld);
 
   return {
     position: vPosition,
-    rotation: vRotation
-  }
+    rotation: vRotation,
+  };
 }
 
-function setCameraData(id){
+// function setCameraData(id) {
+  function setCameraData(savedView) {
   const camera = StoreScene.camera;
-  const savedView = savedViews[id];
+  // const savedView = savedViews[id];
+
+  const middlePoint = {};
+
+  for (const key in savedView.clipping.min) {
+    middlePoint[key] = (savedView.clipping.min + savedView.clipping.max) / 2;
+  }
 
   // set values
+  camera.position.copy(savedView.camera.position);
 
+  camera.lookAt(middlePoint);
 }
 
-export { getCameraData, setCameraData }
+export { getCameraData, setCameraData };
