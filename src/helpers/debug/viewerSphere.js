4440ffa163d1cb122ec6af8a550d11478921384f
MGP-28 <joaosilvapereira96@hotmail.com>
2022-12-19 14:46:31 +0000

    Changed parameters on debug test shape

--- src/helpers/debug/viewerSphere.js
+++ src/helpers/debug/viewerSphere.js
@@ -1,15 +1,17 @@
 import * as SceneStore from "../../stores/scene";
+import * as THREE from 'three'
 
-function testShape() {
+function testShape(position, color) {
   const geometryTest = new THREE.SphereGeometry(0.5, 32, 16);
   const materialTest = new THREE.MeshBasicMaterial({
-    color: 0xffff00,
+    color: color,
     side: THREE.DoubleSide,
+    opacity: 0.3
   });
   const circleTest = new THREE.Mesh(geometryTest, materialTest);
-  circleTest.position.x = 0;
-  circleTest.position.y = 0;
-  circleTest.position.z = 0;
+  circleTest.position.x = position.x;
+  circleTest.position.y = position.y;
+  circleTest.position.z = position.z;
   SceneStore.scene.add(circleTest);
 }
 
