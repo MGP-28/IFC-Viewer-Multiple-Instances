6d9ed415fbfd3886995edacf190b98fb7306fc05
MGP-28 <joaosilvapereira96@hotmail.com>
2022-12-19 14:45:24 +0000

    Added auxiliary functions related to vectors: - Get line between points - Get rotation diference from quarterions - Are vectors equal

---
+++ src/helpers/generic/vectors.js
@@ -0,0 +1,30 @@
+import * as THREE from "three";
+
+function getRotationDiferences(quarterionInitial, quarterionFinal) {
+  const quarterionDif = new THREE.Quaternion();
+
+  for (const axle in quarterionDif) {
+    const angleDiference = quarterionFinal[axle] - quarterionInitial[axle];
+    quarterionDif[axle] = angleDiference;
+  }
+
+  return quarterionDif;
+}
+
+function getLineVector(point1, point2) {
+  const vec = new THREE.Vector3();
+  vec.subVectors(point2, point1);
+  return vec;
+}
+
+function isEqual(v1, v2) {
+  for (const axle in v1) {
+    if (v1[axle] != v2[axle]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+export { getRotationDiferences, getLineVector, isEqual };
