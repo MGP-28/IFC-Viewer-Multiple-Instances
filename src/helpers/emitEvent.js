c90b0babf05b776e99aa40a5f01c9af47fb25b1e
MGP-28 <joaosilvapereira96@hotmail.com>
2022-12-22 15:42:31 +0000

    Added helper to dispatch custom events on specific elements

--- src/helpers/emitEvent.js
+++ src/helpers/emitEvent.js
@@ -1,18 +1,30 @@
-function emitGlobalEvent(eventName){
-    const event = new Event(eventName)
-    document.dispatchEvent(event)
+function emitGlobalEvent(eventName) {
+  const event = new Event(eventName);
+  document.dispatchEvent(event);
 }
 
-function emitCustomGlobalEvent(eventName, detail){
-    const customEvent = new CustomEvent(eventName, {
-        detail: detail
-      });
-    document.dispatchEvent(customEvent)
+function emitCustomGlobalEvent(eventName, detail) {
+  const customEvent = new CustomEvent(eventName, {
+    detail: detail,
+  });
+  document.dispatchEvent(customEvent);
 }
 
 function emitEventOnElement(element, eventName) {
-    const event = new Event(eventName)
-    element.dispatchEvent(event)
+  const event = new Event(eventName);
+  element.dispatchEvent(event);
 }
 
-export { emitGlobalEvent, emitCustomGlobalEvent, emitEventOnElement }
+function emitCustomEventOnElement(element, eventName, detail) {
+  const customEvent = new CustomEvent(eventName, {
+    detail: detail,
+  });
+  element.dispatchEvent(customEvent);
+}
+
+export {
+  emitGlobalEvent,
+  emitCustomGlobalEvent,
+  emitEventOnElement,
+  emitCustomEventOnElement,
+};
