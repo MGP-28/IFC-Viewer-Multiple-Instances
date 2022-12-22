37c9155fe080de76c3a809033d6860a5ce2b5aa1
MGP-28 <joaosilvapereira96@hotmail.com>
2022-12-22 12:58:26 +0000

    Created annotation category store

---
+++ src/stores/annotationCategories.js
@@ -0,0 +1,58 @@
+import { getAnnotationCategories } from "../services/getAnnotationCategories";
+import { saveToLS } from "../services/localStorage";
+
+const annotationCategories = [];
+let id = 0;
+
+// Get saved annotationCategoryCategories
+const savedData = getAnnotationCategories();
+
+if (savedData !== null) {
+  savedData.forEach((annotationCategory) => {
+    annotationCategories.push(annotationCategory);
+  });
+
+  const maxId = Math.max(...annotationCategories.map((x) => x.id));
+  id = maxId;
+}
+
+function addAnnotationCategory(newAnnotationCategory) {
+  id++;
+  const annotationCategory = newAnnotationCategory;
+  annotationCategory.id = id;
+  annotationCategories.push(annotationCategory);
+
+  // trigger event
+  const customEvent = new CustomEvent("newAnnotationCategory", {
+    detail: {
+      annotationCategory: annotationCategory,
+    },
+  });
+  document.dispatchEvent(customEvent);
+
+  saveToLS("annotationCategories", annotationCategories);
+
+  return annotationCategory.id;
+}
+
+function removeAnnotationCategory(id) {
+  const ids = annotationCategories.map((sv) => sv.id);
+  const idx = ids.indexOf(id);
+  if (idx == -1) return;
+  annotationCategories.splice(idx, 1);
+  // trigger event
+  const customEvent = new CustomEvent("updateAnnotationCategoriesList", {
+    detail: {
+      removedId: id,
+    },
+  });
+  document.dispatchEvent(customEvent);
+
+  saveToLS("annotationCategories", annotationCategories);
+}
+
+export {
+  annotationCategories,
+  addAnnotationCategory,
+  removeAnnotationCategory,
+};
