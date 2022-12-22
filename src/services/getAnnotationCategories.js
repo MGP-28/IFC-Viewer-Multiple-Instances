72d619e37137183787600fcca749fd8805afe686
MGP-28 <joaosilvapereira96@hotmail.com>
2022-12-22 12:58:14 +0000

    Created annotation category local storage service

---
+++ src/services/getAnnotationCategories.js
@@ -0,0 +1,21 @@
+import AnnotationCategory from "../models/AnnotationCategory";
+import { loadFromLS } from "./localStorage";
+
+function getAnnotationCategories() {
+  const LSData = loadFromLS("annotationCategories");
+  if (LSData) {
+    const results = [];
+    LSData.forEach((element) => {
+      const annotationCategory = new AnnotationCategory(
+        element.name,
+        element.color
+      );
+      annotationCategory.id = element.id;
+      results.push(annotationCategory);
+    });
+    return results;
+  }
+  return null;
+}
+
+export { getAnnotationCategories };
