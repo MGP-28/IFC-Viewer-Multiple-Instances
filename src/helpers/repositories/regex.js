f2ea3c2404982d116f930dd6007646eafe162105
MGP-28 <joaosilvapereira96@hotmail.com>
2023-01-03 13:29:09 +0000

    Changed annotation category reference validator logic

--- src/helpers/repositories/regex.js
+++ src/helpers/repositories/regex.js
@@ -1,3 +1,3 @@
 export const getIfcRegex = () => /([Ii][Ff][Cc])/g;
 
-export const annotationCategoryRefRegex = () => /^[a-zA-Z]{3}_\d{3}$/; // 3 letters, underscore, 3 numbers
+export const annotationCategoryRefRegex = () => /^[a-zA-Z]{3}-\d{3}$/; // 3 letters, underscore, 3 numbers
