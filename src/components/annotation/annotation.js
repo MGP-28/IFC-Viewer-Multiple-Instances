2671c780fa0848a96aab63b35fa93dd2efab3480
MGP-28 <joaosilvapereira96@hotmail.com>
2023-01-05 12:58:20 +0000

    Deleting an annotation now prompts a confirmation popup

--- src/components/annotation/annotation.js
+++ src/components/annotation/annotation.js
@@ -8,11 +8,13 @@ import { emitEventOnElement } from "../../helpers/emitEvent";
 import { createElement } from "../../helpers/generic/domElements";
 import { removeAnnotation } from "../../stores/annotations";
 import { userInteractions } from "../../stores/userInteractions";
+import { renderConfirmationPopup } from "../confirmationPopup";
 import { buildIcon } from "../generic/icon";
 import {
   addOptionToMenuExtras,
   renderMenuItemExtrasComponent,
 } from "../generic/menuItemExtraOptions";
+import { buildPopupWithHeader } from "../PopupWithHeader";
 
 function renderAnnotation(category, annotation, parent) {
   const element = createElement("li", {
@@ -29,7 +31,8 @@ function renderAnnotation(category, annotation, parent) {
   });
   element.appendChild(text);
 
-  const menuExtras = renderMenuItemExtrasComponent();
+  const eventsToPreventPropagation = ["click"];
+  const menuExtras = renderMenuItemExtrasComponent(eventsToPreventPropagation);
   const options = {
     delete: {
       text: "Delete",
@@ -68,12 +71,13 @@ function renderAnnotation(category, annotation, parent) {
     menuExtras.addEventListener("optionSelected", (e) => {
       const idx = e.detail.idx;
       switch (idx) {
-        case options.delete.idx:
-          deleteAnnotation();
+        case options.delete.idx: {
+          popupConfirmationDeleteAnnotation();
           break;
+        }
 
         default:
-          console.error(`Event handler for index ${idx} is not defined!`)
+          console.error(`Event handler for index ${idx} is not defined!`);
           break;
       }
     });
@@ -110,6 +114,26 @@ function renderAnnotation(category, annotation, parent) {
       remove2DObjectFromScene(label2D);
       element.classList.remove("anim-gradient");
       if (isClick) emitEventOnElement(parent, "childHidden");
+    }
+
+    function popupConfirmationDeleteAnnotation() {
+      const popupProps = {
+        title: "Confirmation",
+        subtitle: "",
+        message: "Are you sure you want to remove the annotation?",
+        affirmativeText: "Remove",
+        negativeText: "Cancel",
+      };
+
+      const popup = renderConfirmationPopup(popupProps, true);
+
+      document.body.appendChild(popup);
+
+      popup.addEventListener("confirmationResult", (e) => {
+        const result = e.detail.result;
+        if(result) deleteAnnotation(annotation);
+        popup.remove();
+      })
     }
 
     function deleteAnnotation() {
