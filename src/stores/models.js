b385111dbf38db809001bb1a151fed2729d43796
MGP-28 <joaosilvapereira96@hotmail.com>
2023-01-06 15:57:17 +0000

    Added boundingBox variable to models store

--- src/stores/models.js
+++ src/stores/models.js
@@ -1,9 +1,9 @@
 // Model class instances
 let models = [];
 const addModel = (model) => {
-  models.push(model)
-  return models.length - 1
-}
+  models.push(model);
+  return models.length - 1;
+};
 const resetModels = () => (models = []);
 function isAllModelsLoaded() {
   for (let idx = 0; idx < models.length; idx++) {
@@ -29,10 +29,17 @@ const getAllCategoriesLoaded = () => {
   return AllCategoriesLoaded;
 };
 
+let boundingBox = undefined;
+const setBoundingBox = (newBoundingBox) => {
+  boundingBox = newBoundingBox;
+};
+
 export {
   models,
   addModel,
   resetModels,
   isAllModelsLoaded,
-  getAllCategoriesLoaded
+  getAllCategoriesLoaded,
+  boundingBox,
+  setBoundingBox,
 };
