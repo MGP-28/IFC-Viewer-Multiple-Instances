35fb71a60a2bf12a4518595c46ee01a2f020f09b
MGP-28 <joaosilvapereira96@hotmail.com>
2023-01-06 12:48:50 +0000

    Fixed issues where JSON parse would convert integers to strings of numbers

--- src/services/getSavedViews.js
+++ src/services/getSavedViews.js
@@ -7,15 +7,37 @@ function getSavedViews() {
   if (LSData) {
     const results = [];
     LSData.forEach((element) => {
-       const savedView = new SavedView();
-       savedView.camera.position = convertJSONToVector(element.camera.position).clone();
-       savedView.camera.pointLookedAt = convertJSONToVector(element.camera.pointLookedAt).clone();
-       savedView.clipping.min = convertJSONToVector(element.clipping.min).clone();
-       savedView.clipping.max = convertJSONToVector(element.clipping.max).clone();
-       savedView.id = element.id;
-       savedView.note = element.note;
-       savedView.hiddenIds = element.hiddenIds;
-       results.push(savedView);
+      const savedView = new SavedView();
+      savedView.camera.position = convertJSONToVector(
+        element.camera.position
+      ).clone();
+      savedView.camera.pointLookedAt = convertJSONToVector(
+        element.camera.pointLookedAt
+      ).clone();
+      savedView.clipping.min = convertJSONToVector(
+        element.clipping.min
+      ).clone();
+      savedView.clipping.max = convertJSONToVector(
+        element.clipping.max
+      ).clone();
+      savedView.id = element.id;
+      savedView.note = element.note;
+
+      // parse values to Int (issue with them being strings and not functioning correctly)
+      for (const modelIdx in element.hiddenIds) {
+        if (Object.hasOwnProperty.call(element.hiddenIds, modelIdx)) {
+          const _hiddenIds = element.hiddenIds[modelIdx];
+          const hiddenIds = _hiddenIds.reduce((pV, cV) => {
+            const num = parseInt(cV);
+            pV.push(num);
+            return pV;
+          }, []);
+          element.hiddenIds[modelIdx] = hiddenIds;
+        }
+      }
+      savedView.hiddenIds = element.hiddenIds;
+
+      results.push(savedView);
     });
     return results;
   }
