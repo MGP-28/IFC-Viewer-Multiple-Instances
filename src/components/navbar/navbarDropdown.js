97d32805fea470176b3946d11d78cfa722d0a681
MGP-28 <joaosilvapereira96@hotmail.com>
2023-01-09 12:13:57 +0000

    Created navbar item dropdown item component

--- src/components/navbar/navbarDropdown.js
+++ src/components/navbar/navbarDropdown.js
@@ -1,9 +1,47 @@
-import { createElement } from "../../helpers/generic/domElements"
+import {
+  emitCustomEventOnElement,
+  emitEventOnElement,
+} from "../../helpers/emitEvent";
+import { createElement } from "../../helpers/generic/domElements";
+import featureRenderingHandler from "../../helpers/navbar/featureRenderingHandler";
+import NavbarItem from "../../models/navbar/NavbarItemData";
 
-function render(){
-    const navbarItemDropdown = createElement("ul", {
-        classes: "feature-navbar-item-dropdown"
-    })
+/**
+ *
+ * @param {NavbarItem} subitem
+ */
+function render(subitem, parent, idx) {
+  const navbarItemDropdown = createElement("li", {
+    classes: ["feature-navbar-item-dropdown-item"],
+    textContent: subitem.title,
+  });
+
+  let isShowing = false;
+  let isRendered = false;
+  handleSubitemEvents();
+
+  return navbarItemDropdown;
+
+  //// Aux functions in scope
+
+  function handleSubitemEvents() {
+    navbarItemDropdown.addEventListener("click", (e) => {
+      const eventName = isShowing ? "subitem-selected" : "subitem-deselected";
+      emitCustomEventOnElement(parent, eventName, { idx });
+    });
+
+    parent.addEventListener("subitem-selected", (e) => {
+      const eventIdx = e.detail.idx;
+      if (eventIdx !== idx) {
+        if (!isRendered || !isShowing) return;
+        isShowing = false;
+      } else isShowing = true;
+      featureRenderingHandler(subitem, isShowing, isRendered);
+      navbarItemDropdown.classList.toggle("active", isShowing);
+    });
+  }
 }
 
-export { render as navbarItemDropdown }
+// handle other item active -> subitem-selected
+
+export { render as renderNavbarItemDropdown };
