ed07c33276f12d26aaeae770725afd46f4ab93f7
MGP-28 <joaosilvapereira96@hotmail.com>
2023-01-09 17:26:17 +0000

    Created sidebar feature component

---
+++ src/components/sidebar/sidebarFeature.js
@@ -0,0 +1,25 @@
+import { icons } from "../../configs/icons";
+import { createElement } from "../../helpers/generic/domElements";
+import NavbarItem from "../../models/navbar/NavbarItemData";
+import { buildIcon } from "../generic/icon";
+
+/**
+ *
+ * @param {NavbarItem} item
+ */
+function render(item) {
+  const mainSidebarFeature = createElement("div", {
+    classes: ["main-sidebar-feature"],
+  });
+
+  mainSidebarFeature.innerHTML = `
+    <span>${item.title}</span>
+    <div class="main-sidebar-feature-content"></div>
+  `;
+  const contentEl = mainSidebarFeature.getElementsByTagName("div")[0];
+
+  const closeIcon = buildIcon(icons.close);
+  mainSidebarFeature.insertBefore(closeIcon, contentEl);
+}
+
+export { render as renderSidebarFeature };
