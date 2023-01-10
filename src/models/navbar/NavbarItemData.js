8ddb56da31c7b993c97e9ca8902f64e248007461
MGP-28 <joaosilvapereira96@hotmail.com>
2023-01-10 12:36:49 +0000

    Model is now resposible for active and rendered statuses

--- src/models/navbar/NavbarItemData.js
+++ src/models/navbar/NavbarItemData.js
@@ -14,8 +14,16 @@ export default class NavbarItem {
    * @param {function} buildFunction handles feature creation
    * @param {function?} loadFunction optional - runs when feature loads to UI
    * @param {function?} unloadFunction optional - runs when feature unloads from UI
+   * @param {boolean?} isExculsive default false - defines if a navbar subitem is exclusively selected
    */
-  constructor(title, isRenderedInSidebar, buildFunction, loadFunction = undefined, unloadFunction = undefined) {
+  constructor(
+    title,
+    isRenderedInSidebar,
+    buildFunction,
+    loadFunction = undefined,
+    unloadFunction = undefined,
+    isExculsive = false
+  ) {
     this.title = title;
     this.isRenderedInSidebar = isRenderedInSidebar;
     this.#buildFunction = buildFunction;
@@ -24,19 +32,26 @@ export default class NavbarItem {
     this.subitems = [];
     this.navbarItem = undefined;
     this.component = undefined;
+    this.isExclusive = isExculsive;
+    this.isRendered = false;
+    this.isActive = false;
   }
 
   build() {
+    // build main component
     if (this.isRenderedInSidebar) {
       const element = renderSidebarFeature(this);
       this.component = element;
     }
-    // build main component
     // build specifics
     this.#buildFunction(this);
+
+    this.isRendered = true;
   }
 
   load() {
+    this.isActive = true;
+
     if (this.#loadFunction !== undefined) this.#loadFunction(this);
 
     emitEventOnElement(this.navbarItem, "loaded");
@@ -49,10 +64,12 @@ export default class NavbarItem {
   }
 
   unload() {
+    this.isActive = false;
+
     if (this.#unloadFunction !== undefined) this.#unloadFunction(this);
 
     emitEventOnElement(this.navbarItem, "unloaded");
-    
+
     // checks if feature is to be rendered in sidebar
     if (!this.isRenderedInSidebar) return;
 
