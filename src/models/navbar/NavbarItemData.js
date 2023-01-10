8f95919e9716f677be0722d2407c1e0fae6fc55b
MGP-28 <joaosilvapereira96@hotmail.com>
2023-01-10 09:57:34 +0000

    Implemented sibebar feature loading/unloading into model class

--- src/models/navbar/NavbarItemData.js
+++ src/models/navbar/NavbarItemData.js
@@ -1,26 +1,44 @@
-export default class NavbarItem{
-    #buildFunction
-    #loadFunction
-    #unloadFunction
-    
-    constructor(title, buildFunction, loadFunction = undefined, unloadFunction = undefined){
-        this.title = title;
-        this.#buildFunction = buildFunction;
-        this.#loadFunction = loadFunction;
-        this.#unloadFunction = unloadFunction;
-        this.subitems = [];
-        this.component = undefined;
-    }
-    build(){
-        const element = this.#buildFunction();
-        this.component = element;
-    }
-    load(){
-        if(this.#loadFunction !== undefined) this.#loadFunction(this.component);
-        // add to sidebar
-    }
-    unload(){
-        if(this.#unloadFunction !== undefined) this.#unloadFunction(this.component);
-        // remove from sidebar
-    }
+import { renderSidebarFeature } from "../../components/sidebar/sidebarFeature";
+import { loadFeatureIntoSidebar, unloadFeatureFromSidebar } from "../../helpers/main-sidebar/mainSidebar";
+
+export default class NavbarItem {
+  #buildFunction;
+  #loadFunction;
+  #unloadFunction;
+
+  /**
+   *
+   * @param {string} title Text to show in UI
+   * @param {function} buildFunction handles feature creation
+   * @param {function?} loadFunction optional - runs when feature loads to UI
+   * @param {function?} unloadFunction optional - runs when feature unloads from UI
+   */
+  constructor(title, buildFunction, loadFunction = undefined, unloadFunction = undefined) {
+    this.title = title;
+    this.#buildFunction = buildFunction;
+    this.#loadFunction = loadFunction;
+    this.#unloadFunction = unloadFunction;
+    this.subitems = [];
+    this.component = undefined;
+  }
+
+  build() {
+    // build main component
+    const element = renderSidebarFeature(this);
+    this.component = element;
+    // build specifics
+    this.#buildFunction(this.component);
+  }
+
+  load() {
+    if (this.#loadFunction !== undefined) this.#loadFunction(this.component);
+    // add to sidebar
+    loadFeatureIntoSidebar(this);
+  }
+
+  unload() {
+    if (this.#unloadFunction !== undefined) this.#unloadFunction(this.component);
+    // remove from sidebar
+    unloadFeatureFromSidebar(this);
+  }
 }
