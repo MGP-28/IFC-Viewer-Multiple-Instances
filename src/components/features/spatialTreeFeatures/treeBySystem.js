2ec2b3e559d9da268aa23800e87af868292db1bc
MGP-28 <joaosilvapereira96@hotmail.com>
2023-01-20 15:16:30 +0000

    Added necessary code for future implementation

--- src/components/features/spatialTreeFeatures/treeBySystem.js
+++ src/components/features/spatialTreeFeatures/treeBySystem.js
@@ -1,5 +1,17 @@
-function render(){
-
+async function render(){
+  // const model = Models.models[leaf.modelIdx];
+  // const props = await model.loader.ifcManager.getItemProperties(0, leaf.expressId);
+  // const psets = await model.loader.ifcManager.getPropertySets(0, leaf.expressId);
+  // for (let index1 = 0; index1 < psets.length; index1++) {
+  //   const pset = psets[index1];
+  //   if (!pset.HasProperties) continue;
+  //   for (let index = 0; index < pset.HasProperties.length; index++) {
+  //     const prop = pset.HasProperties[index];
+  //     if (!prop.value) continue;
+  //     const cc = await model.loader.ifcManager.getItemProperties(0, prop.value);
+  //     console.log("related to", leaf.expressId, cc);
+  //   }
+  // }
 }
 
 export { render as renderSpatialTreeBySystem }
